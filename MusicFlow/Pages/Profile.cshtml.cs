a75c7e5180c8f1c6c64d29ba761bbc18175784ec
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-11-06 20:46:15 +0300

    Added profile & test mockup pages (haven't git added them previously)

---
+++ MusicFlow/Pages/Profile.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MusicFlow.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly ILogger<ProfileModel> _logger;
+
+        public ProfileModel(ILogger<ProfileModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MusicFlow/Pages/Test.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MusicFlow.Pages
+{
+    public class TestModel : PageModel
+    {
+        private readonly ILogger<TestModel> _logger;
+
+        public TestModel(ILogger<TestModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
