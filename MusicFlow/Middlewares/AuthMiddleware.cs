3f16552ef9b6087415f5ba9d9f67cd3edc4c8980
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-01 23:41:06 +0300

    Forgot to add specific files

---
+++ MusicFlow/Middlewares/AuthMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MusicFlow.Middlewares
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            string token = context.Request.Cookies["token"];
+            if (!string.IsNullOrEmpty(token))
+                context.Request.Headers.Add("Authorization", "Bearer " + token);
+
+            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Add("X-Xss-Protection", "1");
+            context.Response.Headers.Add("X-Frame-Options", "DENY");
+
+            return _next(context);
+        }
+    }
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAuthMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthMiddleware>();
+        }
+    }
+}
