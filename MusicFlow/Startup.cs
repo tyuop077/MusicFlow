143632a500ca7e9bf848919a9adee11742fd717e
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-03 22:43:53 +0300

    Added logout controller

--- MusicFlow/Startup.cs
+++ MusicFlow/Startup.cs
@@ -58,6 +58,10 @@ namespace MusicFlow
             services.AddSingleton(new Database(Configuration.GetConnectionString("db")));
 
             services.AddHttpContextAccessor();
+
+            //services.AddMvc(); (used AddControllers instead)
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -92,6 +96,8 @@ namespace MusicFlow
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                //endpoints.MapControllerRoute("default", "{controller=Logout}/{action=Logout}"); (used controllers instead)
             });
         }
     }
