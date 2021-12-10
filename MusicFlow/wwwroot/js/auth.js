f8128c1d88266c9a459d99c824fd84301d4d3a30
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-10 23:00:18 +0300

    Moved duplicate scripts to auth.js

---
+++ MusicFlow/wwwroot/js/auth.js
@@ -0,0 +1,12 @@
+﻿$("#auth").submit(e => {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        data: $("#auth").serialize(),
+        success: data => {
+            if (data.success) window.location.href = "/";
+            $("#tooltip").text(data.error)
+        }
+    });
+});
