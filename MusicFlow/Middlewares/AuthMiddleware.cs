acd683359b8b4111aa4f94315dab47cae62cb6cf
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-16 21:27:05 +0300

    Fixed accidental change authorization header on AuthMiddleware

--- MusicFlow/Middlewares/AuthMiddleware.cs
+++ MusicFlow/Middlewares/AuthMiddleware.cs
@@ -22,7 +22,7 @@ namespace MusicFlow.Middlewares
             {
                 string token = context.Request.Cookies["token"];
                 if (!string.IsNullOrEmpty(token))
-                    context.Response.Headers.Add("Authorization", "Bearer " + token);
+                    context.Request.Headers.Add("Authorization", "Bearer " + token);
 
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
                 context.Response.Headers.Add("X-Xss-Protection", "1");
